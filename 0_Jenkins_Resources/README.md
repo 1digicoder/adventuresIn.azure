07d3335e186dd1101ed44bab4c06fa13c7b27638
Robert Elsner <digicoder@gmail.com>
2018-03-15 23:58:49 -0400

    Initial structure

---
+++ 0_Jenkins_Resources/README.md
@@ -0,0 +1,13 @@
+# Adventures In Azure
+
+## Working through various scenarios and utilities to do fun things in Azure
+
+
+* OSX Utilities
+
+brew install jq
+
+
+* Linux utilities (Debian/Ubuntu)
+
+$ sudo apt-get install jq
---
+++ 0_Jenkins_Resources/destroyResources.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+#set -o xtrace
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
+
+resource_group=`cat ${__dir}/jenkins.json | jq -r '.jenkins.resource_group'`
+
+az group delete -y -n ${resource_group}
